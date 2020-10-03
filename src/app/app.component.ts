b91d7e5423f740949456942e5be235ded4b24017
SheikhAli5 <sheikhsoltanimohammadali@gmail.com>
2020-10-03 10:37:27 +0600

    version 1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{ GlobalConstants } from './common/global-constants';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lab42';
+
+
+   // i: number = 0;
+
+  // ol = (<HTMLInputElement>document.getElementById("ol"));
+
+
+
+  btnClick(b: number) {
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + b;
+
+  };
+
+
+
+  btnClear() {
+    (<HTMLInputElement>document.getElementById("txt")).value = '';
+
+
+  };
+
+
+
+  btnSum() {
+
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + '+';
+
+  };
+
+
+
+  btnSub() {
+
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + "-";
+
+  };
+
+
+
+  btnMult() {
+
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + '*';
+
+  };
+
+
+
+  btnDiv() {
+
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + '/';
+
+  };
+
+
+
+  btnAnswer() {
+
+
+    // let i: number = 0;
+    let ol = (<HTMLInputElement>document.getElementById("ol"));
+    GlobalConstants.i++;
+    (<HTMLInputElement>document.getElementById("txt")).value = (<HTMLInputElement>document.getElementById("txt")).value + "=" + eval((<HTMLInputElement>document.getElementById("txt")).value);
+    let liLast = document.createElement('li');
+    let val: string = (<HTMLInputElement>document.getElementById("txt")).value;
+    liLast.innerHTML = val;
+    (<HTMLInputElement>document.getElementById("txt")).value = '';
+    ol.append(liLast);
+    if (GlobalConstants.i == 4) {
+      ol.removeChild(ol.childNodes[0]);
+      GlobalConstants.i = 3;
+    }
+
+
+  };
+
 }
---
+++ src/app/common/global-constants.ts
@@ -0,0 +1,3 @@
+export class GlobalConstants {
+  public static i=0;
+}
